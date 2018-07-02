4c7ae065ff00454c58d96c49f52519461821ab80
Ivan Ivanov <ivanvelichkov357@gmail.com>
2018-07-02 19:19:27 +0200

    renamed the config js to index js file

--- dbconfig/index.js
+++ dbconfig/index.js
@@ -1,10 +1,10 @@
 //I am just creating a string which will help me connect to the database.
 //It could have been better and more secure, but just for the sake of the exercise.
 
-module.exports = {
-  var username = "ivanwiredelta";
-  var password = "wiredelta03";
+var username = "ivanwiredelta";
+var password = "wiredelta03";
 
+module.exports = {
   getDbConnectionString: function(){
     return 'mongodb://' + username + ':' + password + '@ds125211.mlab.com:25211/nodejswiredelta';
   }
