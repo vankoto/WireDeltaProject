fd9738f86f4ee027fd458f5409d9c01adf706001
Ivan Ivanov <ivanvelichkov357@gmail.com>
2018-07-02 13:38:09 +0200

    Added extra comment and removed the quick test

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
+//I am requiring the express module and then setting up the server port and view engine.
+
 var express = require('express');
 var app = express();
-
 var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 app.set('view engine', 'handlebars');
-
-
-//Just a quick test !
-app.get('/', function(req, res){
-  console.log("Show me the page");
-  res.send("Send me to the browser!");
-});
 
 app.listen(port);
